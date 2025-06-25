0b4bb2b0cffbe31750369cc119ca1affcce23d36
Lukas <lukas@mavenblue.com>
2025-06-25 13:37:24 +0200

    [ZetaMac] With Chart Filtering

--- mental-arithmetic-trainer/main.py
+++ mental-arithmetic-trainer/main.py
@@ -1,4 +1,3 @@
-#main
 import customtkinter as ctk
 import tkinter as tk
 import pandas as pd
@@ -9,7 +8,6 @@ from utils import settings_to_str, plot_graph
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from settings import SettingsFrame
 
 class ZetamacGameApp:
@@ -42,6 +40,22 @@ class ZetamacGameApp:
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.graph_frame)
         self.canvas.get_tk_widget().pack(fill="both", expand=True)
 
+        # Time filter setup
+        self.time_filters = {
+            "Past 1 Month": pd.DateOffset(months=1),
+            "Past 3 Months": pd.DateOffset(months=3),
+            "Past 6 Months": pd.DateOffset(months=6),
+            "Past 1 Year": pd.DateOffset(years=1),
+            "All Time": None
+        }
+        self.time_filter_var = tk.StringVar(value="All Time")
+        ctk.CTkLabel(self.graph_frame, text="Filter by Time Period:").pack(pady=5)
+        self.time_filter_menu = ctk.CTkOptionMenu(self.graph_frame, variable=self.time_filter_var,
+                                                  values=list(self.time_filters.keys()),
+                                                  command=lambda x: self.update_graph())
+        self.time_filter_menu.pack(pady=5)
+
+        # Settings filter setup
         self.filter_var = tk.StringVar(value="All")
         ctk.CTkLabel(self.graph_frame, text="Filter by Settings:").pack(pady=5)
         self.filter_menu = ctk.CTkOptionMenu(self.graph_frame, variable=self.filter_var,
@@ -51,30 +65,40 @@ class ZetamacGameApp:
         self.update_graph()
 
     def update_graph(self):
-        """Update the progression graph based on the filter."""
+        """Update the progression graph based on the time period and settings filters."""
         if not pd.io.common.file_exists("game_results.csv"):
             return
 
         df = pd.read_csv("game_results.csv", parse_dates=["timestamp"])
-        unique_settings = df[["addition", "subtraction", "addition_reverse", "multiplication",
-                              "multiplication_reverse", "add_min", "add_max", "mul_min",
-                              "mul_max", "duration"]].drop_duplicates()
 
+        # Filter by time period
+        selected_time_period = self.time_filter_var.get()
+        if selected_time_period != "All Time":
+            offset = self.time_filters[selected_time_period]
+            start_date = pd.Timestamp.now() - offset
+            df_filtered = df[df['timestamp'] >= start_date]
+        else:
+            df_filtered = df
+
+        # Get unique settings from the filtered dataframe
+        unique_settings = df_filtered[["addition", "subtraction", "addition_reverse", "multiplication",
+                                       "multiplication_reverse", "add_min", "add_max", "mul_min",
+                                       "mul_max", "duration"]].drop_duplicates()
         self.settings_dict = {}
         for _, row in unique_settings.iterrows():
             settings_str = settings_to_str(row)
             self.settings_dict[settings_str] = row.to_dict()
-
         settings_list = ["All"] + list(self.settings_dict.keys())
         self.filter_menu.configure(values=settings_list)
 
-        plot_graph(self.ax, df, self.settings_dict, self.filter_var.get())
+        # Get selected settings and plot
+        selected_settings = self.filter_var.get()
+        plot_graph(self.ax, df_filtered, self.settings_dict, selected_settings)
         self.canvas.draw()
 
     def run(self):
         """Start the application."""
         self.root.mainloop()
-
 
 if __name__ == "__main__":
     app = ZetamacGameApp()
